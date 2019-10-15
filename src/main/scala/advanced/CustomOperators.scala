010f9ee0f8a3344c07d15572e85897afd77bf25e
Kirill <immotional@aol.com>
2019-10-15 20:04:31 +0100

    custom flows

--- src/main/scala/advanced/CustomOperators.scala
+++ src/main/scala/advanced/CustomOperators.scala
@@ -1,16 +1,18 @@
 package advanced
 
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.{Sink, Source}
-import akka.stream.{ActorMaterializer, Attributes, Inlet, Outlet, SinkShape, SourceShape}
-import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
+import akka.stream.scaladsl.{Flow, Sink, Source}
+import akka.stream.{ActorMaterializer, Attributes, FlowShape, Inlet, Outlet, SinkShape, SourceShape}
+import akka.stream.stage.{GraphStage, GraphStageLogic, GraphStageWithMaterializedValue, InHandler, OutHandler}
 
 import scala.collection.mutable
+import scala.concurrent.{Future, Promise}
 import scala.util.Random
 
 object CustomOperators extends App {
   implicit val system = ActorSystem("custom-operators")
   implicit val materializer = ActorMaterializer()
+  import system.dispatcher
 
   // custom source which emits random numbers until cancelled
   class RandomNumberGenerator(max: Int) extends GraphStage[SourceShape[Int]] {
@@ -45,6 +47,7 @@ object CustomOperators extends App {
         override def onPush(): Unit = {
           val nextElement = grab(inport)
           batch.enqueue(nextElement)
+          Thread.sleep(100)
           if (batch.size >= batchSize) {
             println(s"new batch ${batch.dequeueAll(_ => true).mkString("[", ",", "]")}")
           }
@@ -62,5 +65,74 @@ object CustomOperators extends App {
 
   val batcherSink = Sink.fromGraph(new BatchSink[Int](10))
 
-  randomNumberGenerator.to(batcherSink).run()
+//  randomNumberGenerator.to(batcherSink).run()
+
+  // custom filter flow
+  class FilterFlow[T](predicate: T => Boolean) extends GraphStage[FlowShape[T, T]] {
+    private val input = Inlet[T]("fliterFlowInput")
+    private val output = Outlet[T]("fliterFlowOutput")
+    override def shape: FlowShape[T, T] = FlowShape[T, T](input, output)
+
+    override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new GraphStageLogic(shape) {
+      setHandlers(input, output, new InHandler with OutHandler {
+        override def onPush(): Unit = {
+          try {
+            val element = grab(input)
+            if (predicate(element)) {
+              push(output, element)
+            } else {
+              pull(input)
+            }
+          } catch {
+            case e: Throwable => failStage(e)
+          }
+        }
+
+        override def onPull(): Unit = pull(input)
+      })
+    }
+  }
+
+  val filterFlow = Flow.fromGraph(new FilterFlow[Int](_ > 80))
+
+//  randomNumberGenerator.via(filterFlow).to(batcherSink).run()
+
+  // materialized values in graph stages
+  class CounterFlow[T]() extends GraphStageWithMaterializedValue[FlowShape[T, T], Future[Int]] {
+    private val input = Inlet[T]("counterFlowInput")
+    private val output = Outlet[T]("counterFlowOutput")
+    override def shape: FlowShape[T, T] = FlowShape[T, T](input, output)
+
+    override def createLogicAndMaterializedValue(inheritedAttributes: Attributes): (GraphStageLogic, Future[Int]) = {
+      val promise = Promise[Int]
+      val logic = new GraphStageLogic(shape) {
+        var counter = 0
+        setHandlers(input, output, new InHandler with OutHandler {
+          override def onPush(): Unit = {
+            val element = grab(input)
+            counter += 1
+            push(output, element)
+          }
+
+          override def onPull(): Unit = pull(input)
+
+          override def onDownstreamFinish(): Unit = {
+            promise.success(counter)
+            super.onDownstreamFinish()
+          }
+
+          override def onUpstreamFinish(): Unit = {
+            promise.success(counter)
+            super.onUpstreamFinish()
+          }
+
+          override def onUpstreamFailure(ex: Throwable): Unit = {
+            promise.failure(ex)
+            super.onUpstreamFailure(ex)
+          }
+        })
+      }
+      (logic, promise.future)
+    }
+  }
 }
